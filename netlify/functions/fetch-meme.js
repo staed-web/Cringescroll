23953476836c2174e8aa64a0e57c70cc067f3481
staed-web <darshit.kathiriya02@gmail.com>
2025-09-16 21:31:20 +0530

    Update fetch-meme.js

--- netlify/functions/fetch-meme.js
+++ netlify/functions/fetch-meme.js
@@ -1,30 +1,31 @@
 import fetch from 'node-fetch';
 
-const imgurEndpoints = [
-  'https://api.imgur.com/3/gallery/random',
-  'https://api.imgur.com/3/gallery/hot'
-];
+const subreddits = ["cringetopia", "teenagers", "facebookstories"];
+const randomSub = subreddits[Math.floor(Math.random() * subreddits.length)];
 
-exports.handler = async () => {
-  const endpoint = imgurEndpoints[Math.floor(Math.random() * imgurEndpoints.length)];
-  
+export async function handler() {
   try {
-    const res = await fetch(endpoint, {
+    const res = await fetch(`https://www.reddit.com/r/${randomSub}/hot.json?limit=1`, {
       headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
       }
     });
 
     if (!res.ok) throw new Error(`HTTP ${res.status}`);
-    
+
     const data = await res.json();
-    if (!data.data || !data.data[0].link) throw new Error("Invalid response");
+    const post = data.data.children[0].data;
+
+    // Only allow image links
+    if (!post.url.includes("i.redd.it")) {
+      throw new Error("Not an image");
+    }
 
     return {
       statusCode: 200,
       body: JSON.stringify({
-        url: data.data[0].link,
-        title: data.data[0].title || "Random Cringe"
+        url: post.url,
+        title: post.title
       })
     };
   } catch (err) {
@@ -37,4 +38,4 @@ exports.handler = async () => {
       })
     };
   }
-};
+}
